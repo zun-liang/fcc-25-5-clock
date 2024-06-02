aa88eca02934bc0ff329685fffc8cbf7eabca548
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-02 16:13:46 -0500

    update Timer functions

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const TimerContainer = styled.div`
@@ -17,14 +18,62 @@ const StyledP = styled.p`
   font-weight: bold;
 `;
 const Time = styled.p`
-  font-size: 4rem;
+  font-size: 4.5rem;
 `;
 
-const Timer = () => {
+interface TimeProps {
+  start: boolean;
+  breakLength: number;
+  sessionLength: number;
+}
+const Timer = ({ start, breakLength, sessionLength }: TimeProps) => {
+  const [onBreak, setOnBreak] = useState(false);
+  const [sessionSeconds, setSessionSeconds] = useState(sessionLength * 60);
+  const [breakSeconds, setBreakSeconds] = useState(breakLength * 60);
+
+  useEffect(() => {
+    setSessionSeconds(sessionLength * 60);
+  }, [sessionLength]);
+
+  useEffect(() => {
+    setBreakSeconds(breakLength * 60);
+  }, [breakLength]);
+
+  const timeLeft = () => {
+    let minutes = Math.floor(sessionSeconds / 60)
+      .toString()
+      .padStart(2, "0");
+    let seconds = (sessionSeconds % 60).toString().padStart(2, "0");
+    const formattedTimeLeft = minutes + ":" + seconds;
+    if (start && formattedTimeLeft === "00:00") {
+      setOnBreak(true);
+      minutes = Math.floor(breakSeconds / 60)
+        .toString()
+        .padStart(2, "0");
+      seconds = (breakSeconds % 60).toString().padStart(2, "0");
+    }
+    return formattedTimeLeft;
+
+  };
+
+  useEffect(() => {
+    if (start && !onBreak) {
+      setInterval(() => {
+        setSessionSeconds((prev) => prev - 1);
+      }, 1000);
+    } else if (start && onBreak) {
+      setInterval(() => {
+        setBreakSeconds(prev => prev - 1);
+      }, 1000)
+    }
+  }, [start, onBreak]);
+
   return (
     <TimerContainer>
-      <StyledP id="timer-label">Session</StyledP>
-      <Time>25:00</Time>
+      <StyledP id="timer-label">
+        {onBreak ? "Break" : "Session"}
+      </StyledP>
+      <Time id="time-left">{timeLeft()}</Time>
     </TimerContainer>
   );
 };

ab2164bd0c2b1b73a436754cc056f17c0f0fe684
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-02 16:13:29 -0500

    update Clock functions

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -15,7 +15,7 @@ const ClockContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   justify-items: center;
-  row-gap: 2rem;
+  row-gap: 2.5rem;
   column-gap: 1rem;
 `;
 const Title = styled.h1`
@@ -28,13 +28,18 @@ const LengthContainer = styled.div`
   display: grid;
   grid-template-columns: 2rem 1fr 2rem;
   justify-items: center;
-  gap: 1rem;
+  align-items: center;
+  row-gap: 1rem;
+  column-gap: 0.5rem;
 `;
 const StyledLabel = styled.label`
   grid-column: 1 / -1;
   justify-self: center;
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: bold;
+  @media (min-width: 1200px) {
+    font-size: 2rem;
+  }
 `;
 const StyledInput = styled.input`
   border: none;
@@ -42,11 +47,19 @@ const StyledInput = styled.input`
   background-color: orange;
   border-radius: 1rem;
   width: 5rem;
-  height: 2rem;
+  height: 2.5rem;
   font-size: 1.5rem;
+  text-align: center;
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  -moz-appearance: textfield;
 `;
 const Arrow = styled(FontAwesomeIcon)`
   font-size: 1.5rem;
+  cursor: pointer;
 `;
 const IconsContainer = styled.div`
   grid-column: 1 / 3;
@@ -54,30 +67,57 @@ const IconsContainer = styled.div`
 const Icon = styled(FontAwesomeIcon)`
   font-size: 2rem;
   margin: 1rem;
+  cursor: pointer;
 `;
 
 const Clock = () => {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const updateBreakLength = (e: React.FormEvent<HTMLInputElement>): void =>
-    setBreakLength(parseInt(e.currentTarget.value));
-  const updateSessionLength = (e: React.FormEvent<HTMLInputElement>): void =>
-    setSessionLength(parseInt(e.currentTarget.value));
+  const [start, setStart] = useState(false);
+  const handleStart = () => setStart(true);
+  const handlePause = () => setStart(false);
+  const handleReset = () => {
+    setStart(false);
+    setBreakLength(5);
+    setSessionLength(25);
+  };
+
+  const updateBreakLength = (e: React.FormEvent<HTMLInputElement>): void => {
+    const breakInput = e.currentTarget.value;
+    if (breakInput) {
+      setBreakLength(parseInt(breakInput));
+    } else {
+      setBreakLength(0);
+    }
+  };
+
+  const updateSessionLength = (e: React.FormEvent<HTMLInputElement>): void => {
+    const sessionInput = e.currentTarget.value;
+    if (sessionInput) {
+      setSessionLength(parseInt(sessionInput));
+    } else {
+      setSessionLength(0);
+    }
+  };
+
   const incrementBreak = () => {
-    if (breakLength < 59) {
+    if (breakLength < 60) {
       setBreakLength((prev) => prev + 1);
     }
   };
+
   const decrementBreak = () => {
     if (breakLength >= 1) {
       setBreakLength((prev) => prev - 1);
     }
   };
+
   const incrementSession = () => {
-    if (sessionLength < 59) {
+    if (sessionLength < 60) {
       setSessionLength((prev) => prev + 1);
     }
   };
+
   const decrementSession = () => {
     if (sessionLength >= 1) {
       setSessionLength((prev) => prev - 1);
@@ -94,6 +134,10 @@ const Clock = () => {
         <Arrow icon={faArrowUp} id="break-increment" onClick={incrementBreak} />
         <StyledInput
           id="break-length"
+          name="break-length"
+          type="number"
+          min="1"
+          max="60"
           value={breakLength}
           onChange={updateBreakLength}
         />
@@ -114,6 +158,10 @@ const Clock = () => {
         />
         <StyledInput
           id="session-length"
+          name="session-length"
+          type="number"
+          min="1"
+          max="60"
           value={sessionLength}
           onChange={updateSessionLength}
         />
@@ -123,11 +171,15 @@ const Clock = () => {
           onClick={decrementSession}
         />
       </LengthContainer>
-      <Timer />
+      <Timer
+        start={start}
+        breakLength={breakLength}
+        sessionLength={sessionLength}
+      />
       <IconsContainer>
-        <Icon icon={faPlay} id="start_stop" />
-        <Icon icon={faPause} />
-        <Icon icon={faArrowsRotate} id="reset" />
+        <Icon icon={faPlay} id="start_stop" onClick={handleStart} />
+        <Icon icon={faPause} onClick={handlePause} />
+        <Icon icon={faArrowsRotate} id="reset" onClick={handleReset} />
       </IconsContainer>
     </ClockContainer>
   );

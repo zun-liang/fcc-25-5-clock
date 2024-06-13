63c0fc4a423d086b7aabce98fdf6cddb1c61c3f4
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-12 21:32:19 -0500

    downgrade react to 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
+// import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );

84e9e6e766512ac21396427141d605e62895e5fa
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-06-23 19:31:05 -0500

    update to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// import App from "./App.tsx";
+import App from "./App.tsx";
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
